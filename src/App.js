59cceed9355101b1cf81842a4be8502d17c921e6
brt <dubudabu@gmail.com>
2022-09-07 08:08:31 +0200

    Add JQueryUI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.datePickerContainer = React.createRef()
+  }
+  componentDidMount() {
+    window.$(this.datePickerContainer.current).datepicker({
+      onSelect: this.props.onDateChange
+    })
+  }
+  componentWillUnmount() {
+    window.$(this.datePickerContainer.current).datepicker("destroy")
+  }
+  render() {
+    return <div ref={this.datePickerContainer} />
+  }
+}
+
+class App extends React.Component {
+  state = {
+    selectedDate: null
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello JQuery</h1>
+        <h2>{this.state.selectedDate ? this.state.selectedDate : "Please select a date."} </h2>
+        <DatePicker onDateChange={(date => {
+          this.setState({
+            selectedDate: date
+          })
+        })} />
+      </div>
+    );
+  }
 }
 
 export default App;
+
