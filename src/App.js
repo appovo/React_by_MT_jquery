ec82e5d13da739d81701f0898268648465a51f84
brt <dubudabu@gmail.com>
2022-09-08 07:37:24 +0200

    W6L5 homework: add initial date to datepicker

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import ProgressBarJui from './ProgressBarJui';
 
 class DatePicker extends React.Component {
+
   constructor(props) {
     super(props)
     this.datePickerContainer = React.createRef()
   }
   componentDidMount() {
     window.$(this.datePickerContainer.current).datepicker({
-      onSelect: this.props.onDateChange
+      onSelect: this.props.onDateChange,
+      defaultDate: this.props.initialDate
     })
   }
   componentWillUnmount() {
@@ -22,21 +24,23 @@ class DatePicker extends React.Component {
 
 class App extends React.Component {
   state = {
-    selectedDate: null
+    selectedDate: null,
+    initialDate: '01/01/2000'
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hello JQuery</h1>
-        <h2>{this.state.selectedDate ? this.state.selectedDate : "Please select a date."} </h2>
+        <h2>{this.state.selectedDate ? this.state.selectedDate : this.state.initialDate} </h2>
         <DatePicker onDateChange={(date => {
           this.setState({
             selectedDate: date
           })
-        })} />
+        })}
+          initialDate={this.state.initialDate} />
         <br />
-        <ProgressBarJui percent={33} />
+        <ProgressBarJui percent={50} />
       </div>
     );
   }
